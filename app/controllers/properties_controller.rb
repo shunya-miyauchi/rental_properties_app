a9a5eb926e69b42562e84dc091d418d84e4138ee
Miyauchi <hy.miyauchi@gmail.com>
2022-02-17 23:24:39 +0900

    Add generate-properties

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,34 @@
+class PropertiesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+
+end
